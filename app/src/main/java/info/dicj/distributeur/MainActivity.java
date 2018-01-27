1ae4910e337de269b0e0cba3bea41484e510bf34
Evilermine <35575424+Evilermine@users.noreply.github.com>
2018-01-26 19:40:55 -0500

    widgets
    manque juste checkbox

--- app/src/main/java/info/dicj/distributeur/MainActivity.java
+++ app/src/main/java/info/dicj/distributeur/MainActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import info.dicj.distributeur.Distributeur.Distributeur;
 import info.dicj.distributeur.Distributeur.Exception.AucunDistribuableException;
@@ -13,7 +19,7 @@ import info.dicj.distributeur.Distributeur.Exception.AucunMelangeException;
 import info.dicj.distributeur.Distributeur.Exception.DebordementMelangeException;
 import info.dicj.distributeur.Distributeur.Recette;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnCheckedChangeListener {
 
     private Distributeur distributeur;
 
@@ -22,9 +28,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ((RadioGroup)findViewById(R.id.group)).setOnCheckedChangeListener(this);
+        ((CheckBox)findViewById(R.id.check)).setOnCheckedChangeListener(this);
+
         distributeur = new Distributeur();
 
         Log.i("DICJ", "MainActivity.oncreate");
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId){
+        TextView adore = new TextView(this);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linear1);
+        EditText nom = (EditText) findViewById(R.id.nom);
+
+        switch(checkedId)
+        {
+            case R.id.adore:
+                adore.setText("Bravo " + nom.getText() + "! Merci de ton intérêt pour le cours !");
+                adore.setTextSize(12);
+                adore.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                layout.addView(adore, layout.indexOfChild(group)+1);
+                break;
+            case R.id.adorepas:
+                adore.setText("Continue tes efforts " + nom.getText() + ", tout ira bien !");
+                adore.setTextSize(12);
+                adore.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                layout.addView(adore, layout.indexOfChild(group)+1);
+                break;
+        }
+
+        for(int i = 0; i < group.getChildCount(); i++)
+            group.getChildAt(i).setEnabled(false);
     }
 
     public void reverser(View view) {
@@ -122,5 +157,4 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, information, duree);
         toast.show();
     }
-
 }
